b723d349742961e62516ae960ecef5e04e8f95c2
Philipp Wolfer <phil@parolu.io>
2019-05-23 07:51:50 +0200

    Removed tutorial comments from build.rs
    The comments where directly from the bindgen tutorial and where somewhat redundant (the code is rather self explanatory)

--- build.rs
+++ build.rs
@@ -4,26 +4,16 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    // Tell cargo to tell rustc to link the system bzip2
-    // shared library.
     println!("cargo:rustc-link-lib=discid");
 
-    // The bindgen::Builder is the main entry point
-    // to bindgen, and lets you build up options for
-    // the resulting bindings.
     let bindings = bindgen::Builder::default()
-        // The input header we would like to generate
-        // bindings for.
         .header("discid.h")
-        // Extra configuration for libdiscid
         .constified_enum_module("discid_feature")
         .blacklist_item("DISCID_VERSION_MAJOR")
         .blacklist_item("DISCID_VERSION_MINOR")
         .blacklist_item("DISCID_VERSION_PATCH")
         .blacklist_item("DISCID_VERSION_NUM")
-        // Finish the builder and generate the bindings.
         .generate()
-        // Unwrap the Result and panic on failure.
         .expect("Unable to generate bindings");
 
     // Write the bindings to the $OUT_DIR/bindings.rs file.
